c31b3aeab47d36d54241b05ba16772b774fa123e
Chickn <chicknbao@protonmail.com>
2025-05-07 13:19:08 +0100

    Add staging environment configuration

--- next.config.js
+++ next.config.js
@@ -7,6 +7,14 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  env: {
+    NEXT_PUBLIC_APP_ENV: process.env.NEXT_PUBLIC_APP_ENV || "production",
+  },
+  basePath: process.env.NEXT_PUBLIC_APP_ENV === "staging" ? "/staging" : "",
 };
 
 module.exports = nextConfig;
